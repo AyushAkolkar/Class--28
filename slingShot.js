1515d44bcdfe82c33ecbc872ec0b13dae0243981
AyushAkolkar <70316658+AyushAkolkar@users.noreply.github.com>
2020-12-28 14:58:56 +0530

    Add files via upload

---
+++ slingShot.js
@@ -0,0 +1,26 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB=pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    display(){
+        if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+  fly(){
+      this.sling.bodyA=null;
+  }
+    
+}
